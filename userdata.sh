2919dcb3f51c1aee4d6f4f08636a3b43ff7cb37b
Aliyah Gamble <shardaegamble@gmail.com>
2025-02-12 23:20:06 -0500

    Updated userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 # Install Docker
 sudo yum update -y
 sudo yum install -y docker
@@ -8,9 +7,9 @@ sudo usermod -aG docker ec2-user
 
 # Pull Docker image (if hosted on ECR)
 # aws ecr get-login-password | docker login --username AWS 
---password-stdin <AWS_ACCOUNT_ID>.dkr.ecr.us-east-1.amazonaws.com
+--password-stdin 976193234775.dkr.ecr.us-east-1.amazonaws.com
 # docker pull 
-<AWS_ACCOUNT_ID>.dkr.ecr.us-east-1.amazonaws.com/rails-app:latest
+976193234775.dkr.ecr.us-east-1.amazonaws.com/rails-app:latest
 
 # Build and run the Rails app
 docker run -d -p 3000:3000 rails-app
